4f080004478fc6bd713ced0ac556b79b2ca743f6
Andreas Christoffersen <aoch21@student.aau.dk>
2024-03-12 20:22:26 +0100

    Add linked list creation and sort task
    A task comment was added to the top of the main function in linkedList.go to create a linked list from a set of values and then sort that list. This serves as a reminder for future implementation.

--- linkedListTrolling/linkedList.go
+++ linkedListTrolling/linkedList.go
@@ -1,5 +1,6 @@
 package linkedListTrolling
 
+// ToDo: Have a set of values be transformed into a linked list, and then sort the linked list.
 func main() {
-
+	valueArray := [10]int{1, 8, 3, 5, 1, 6, 2, 0, 5, 1}
 }
