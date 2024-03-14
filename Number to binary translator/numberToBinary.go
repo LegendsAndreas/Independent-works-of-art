a18c2e7a4f31951f85eb12ca3a9cb8335a0f7ec9
Andreas <aoch21@student.aau.dk>
2024-03-14 11:01:22 +0100

    1.0

--- Number to binary translator/numberToBinary.go
+++ Number to binary translator/numberToBinary.go
@@ -1,5 +1,79 @@
 package main
 
+import "fmt"
+
 func main() {
-	var number int = 256
+	var number int = 69
+	fmt.Println(numToBi(number))
+
+}
+
+func numToBi(num int) string {
+	binaryStr := ""
+	hun := 128
+	sixtyfour := 64
+	thirtytwo := 32
+	sixteen := 16
+	eight := 8
+	four := 4
+	two := 2
+	one := 1
+
+	if num >= hun {
+		binaryStr += "1"
+		num -= hun
+	} else {
+		binaryStr += "0"
+	}
+
+	if num >= sixtyfour {
+		binaryStr += "1"
+		num -= sixtyfour
+	} else {
+		binaryStr += "0"
+	}
+
+	if num >= thirtytwo {
+		binaryStr += "1"
+		num -= thirtytwo
+	} else {
+		binaryStr += "0"
+	}
+
+	if num >= sixteen {
+		binaryStr += "1"
+		num -= sixteen
+	} else {
+		binaryStr += "0"
+	}
+
+	if num >= eight {
+		binaryStr += "1"
+		num -= eight
+	} else {
+		binaryStr += "0"
+	}
+
+	if num >= four {
+		binaryStr += "1"
+		num -= four
+	} else {
+		binaryStr += "0"
+	}
+
+	if num >= two {
+		binaryStr += "1"
+		num -= two
+	} else {
+		binaryStr += "0"
+	}
+
+	if num >= one {
+		binaryStr += "1"
+		num -= one
+	} else {
+		binaryStr += "0"
+	}
+
+	return binaryStr
 }
