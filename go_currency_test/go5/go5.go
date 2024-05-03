e2407ef68bddc25a4ab68c84e75dd385424bded4
Andreas <aoch21@student.aau.dk>
2024-05-03 11:09:11 +0200

    Refactor loop iterations and adjust file open permissions
    The main changes in the commit include decreasing the iterations in multiple loops from 250_000 to 200_000 in the go5.go file. The commit also altered the mode of opening 'goCun5Records.txt' from both writing and reading

--- go_currency_test/go5/go5.go
+++ go_currency_test/go5/go5.go
@@ -15,35 +15,35 @@ func main() { // Gorutine 1
 	start := time.Now()
 	wg.Add(5)
 	go func() {
-		for i := 0; i < 250_000; i++ {
+		for i := 0; i < 200_000; i++ {
 			fmt.Println(i)
 		}
 		wg.Done()
 	}()
 
 	go func() { // Gorutine 2
-		for i := 0; i < 250_000; i++ {
+		for i := 0; i < 200_000; i++ {
 			fmt.Println(i)
 		}
 		wg.Done()
 	}()
 
 	go func() { // Gorutine 3
-		for i := 0; i < 250_000; i++ {
+		for i := 0; i < 200_000; i++ {
 			fmt.Println(i)
 		}
 		wg.Done()
 	}()
 
 	go func() { // Gorutine 4
-		for i := 0; i < 250_000; i++ {
+		for i := 0; i < 200_000; i++ {
 			fmt.Println(i)
 		}
 		wg.Done()
 	}()
 
 	go func() { // Gorutine 5
-		for i := 0; i < 250_000; i++ {
+		for i := 0; i < 200_000; i++ {
 			fmt.Println(i)
 		}
 		wg.Done()
@@ -55,7 +55,7 @@ func main() { // Gorutine 1
 	fmt.Println(time.Since(start))
 
 	// We open the text file 'goCun1Records.txt', in the append mode, so that we can add other values to get, rather than just create a new value.
-	file, err := os.OpenFile("goCun5Records.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("goCun5Records.txt", os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
